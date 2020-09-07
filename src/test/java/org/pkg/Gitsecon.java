83ee60df1c8ef686e75445086ad93190c25c5d2a
gopianandh <gopinathan2296@gmal.com>
2020-09-07 21:25:12 +0530

    to C

--- src/test/java/org/pkg/Gitsecon.java
+++ src/test/java/org/pkg/Gitsecon.java
@@ -7,16 +7,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Gitsecon {
 	public static void main(String[] args) {
 		
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\Dorai\\eclipse-workspace\\Gitsecondd\\div\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-	     driver.get("https://www.youtube.com/");
-	     driver.findElement(By.xpath("(//a[@id='video-title-link'])[2]")).click();
-	     
-	     for (int i=0; i<10;i++) {
-	    	 System.out.println(i);
+	System.out.println("success");
 	     }
 	     
 	     
 	}
 
-}
+
